4a974ccc44add4c5c3dbca59490c81fc104a5e1b
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-01-21 22:04:35 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -29,8 +29,8 @@ TW_LOAD_VENDOR_MODULES := "goodix_fp.ko  modules.load modules.load.recovery msm_
  
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery
 
 PRODUCT_PACKAGES += \
     bootctrl \
